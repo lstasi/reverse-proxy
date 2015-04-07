d82950ef2008500e530ad5f888f4a7f2fb2ac40a
Leandro <lstasi@hotmail.com>
2015-04-08 00:17:29 +0200

    Fix puppet provision folder

--- provision/install_puppet.sh
+++ provision/install_puppet.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-if [ ! -f "/vagrant/.puppet_installed" ]
+if [ ! -f "/.provision/.puppet_installed" ]
 then
   echo "Install puppet repository"
 	sudo rpm -ivh https://yum.puppetlabs.com/el/6/products/x86_64/puppetlabs-release-6-7.noarch.rpm
 	echo "Install puppet"
 	sudo yum install puppet -y
-	touch /vagrant/.puppet_installed
+	mkdir -p /.provision
+	touch /.provision/.puppet_installed
 fi
 echo "Puppet already installed"
 
