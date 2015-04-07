f3d176730e0d7b7509b579997940690dab4e2d98
Leandro <lstasi@hotmail.com>
2015-04-07 23:35:07 +0200

    Fix getServers, add puppet dep

--- getServers.sh
+++ getServers.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 set -e
 VERBOSE="--silent"
-while getopts ":v:h" opt; do
+DOCKERSERVER="127.0.0.1"
+DOCKERPORT="80"
+while getopts ":vs:p:h" opt; do
   case $opt in
     v)
       VERBOSE="--verbose"
       ;;
+	s)
+      DOCKERSERVER=$OPTARG
+      ;;
+	p)
+      DOCKERPORT=$OPTARG
+      ;;
 	h)
-      echo "Run getServers.sh -v for verbose" >&2
+	  echo "-s DOCKERSERVER ip address"
+	  echo "-p DOCKERPORT port" 
+      echo "Run getServers.sh -v for verbose"
       exit 1
       ;;
     \?)
@@ -16,14 +26,16 @@ while getopts ":v:h" opt; do
       ;;
   esac
 done
-
+echo "$DOCKERSERVER:$DOCKERPORT"
 echo -e "\nLoad DataCenters\n"
-curl $VERBOSE -u 3scale:test --header 'Host: datacenters-api.va.3sca.net' 'http://127.0.0.1:80/datacenters/' -o temp/datacenters.json
-
-dc_id=($(jq -r '.[].id' temp/datacenters.json))
-dc_name=($(jq -r '.[].name' temp/datacenters.json))
-dc_servers=($(jq -r '.[].servers' temp/datacenters.json))
-dc_location=($(jq -r '.[].location' temp/datacenters.json))
+curl $VERBOSE -u 3scale:test --header 'Host: datacenters-api.va.3sca.net' "http://$DOCKERSERVER:$DOCKERPORT/datacenters/" -o /tmp/datacenters.json
+if [ "$VERBOSE" == "--verbose" ]; then
+	jq -r '.[]' /tmp/datacenters.json
+fi
+dc_id=($(jq -r '.[].id' /tmp/datacenters.json))
+dc_name=($(jq -r '.[].name' /tmp/datacenters.json))
+dc_servers=($(jq -r '.[].servers' /tmp/datacenters.json))
+dc_location=($(jq -r '.[].location' /tmp/datacenters.json))
 total=${#dc_id[*]}
 
 echo "Total DataCenters : ${#dc_id[*]}"
@@ -35,15 +47,18 @@ do
 	server_list="${dc_servers[$i]}"
 	for server in $server_list
 	do
-		curl $VERBOSE -u 3scale:test --header 'Host: servers-api.va.3sca.net' "http://127.0.0.1:80/servers/$server" -o temp/$server.json
-		srv_code=($(jq -r '.code' temp/$server.json))
+		curl $VERBOSE -u 3scale:test --header 'Host: servers-api.va.3sca.net' "http://$DOCKERSERVER:$DOCKERPORT/servers/$server" -o /tmp/$server.json
+		if [ "$VERBOSE" == "--verbose" ]; then
+			jq -r '.' /tmp/$server.json
+		fi
+		srv_code=($(jq -r '.code' /tmp/$server.json))
 		if (( "$srv_code"=="1" )); then
-			srv_message=($(jq -r '.message' temp/$server.json))
+			srv_message=($(jq -r '.message' /tmp/$server.json))
 			echo -e "Server: Error - ID: $server code: $srv_code message: $srv_message"
 		else
-			srv_id=($(jq -r '.id' temp/$server.json))
-			srv_name=($(jq -r '.name' temp/$server.json))
-			srv_desc=($(jq -r '.description' temp/$server.json))
+			srv_id=($(jq -r '.id' /tmp/$server.json))
+			srv_name=($(jq -r '.name' /tmp/$server.json))
+			srv_desc=($(jq -r '.description' /tmp/$server.json))
 			echo -e "Server: $srv_name: $srv_desc"
 		fi
 	done
--- modules/reverse_proxy/manifests/getservers.pp
+++ modules/reverse_proxy/manifests/getservers.pp
@@ -1,3 +1,9 @@
 class reverse_proxy::getservers{
-	package { ["epel-release","dockers","curl","jq"]: ensure => present }
+	package { ["epel-release"]: ensure => present }
+	package { ["docker-io","curl","jq"]: 
+				ensure => present,
+				require => ["epel-release"]}
+	service { "docker":
+			   ensure => "running"
+			} 
 }
